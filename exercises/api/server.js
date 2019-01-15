d05e47a5049dd02cdbb1f18fe15c1685932bb5df
McNamara <cmcnamara@cmcnamarambpxx.cnet.cnwk>
2019-01-14 16:53:05 -0800

    my solution for API exercise

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -18,21 +18,42 @@ const port = 3000
 // log incoming request coming into the server. Helpful for debugging and tracking
 const logRequest = (method, route, status) => console.log(method, route, status)
 
+const router = {
+  '/': {
+    'file':'index.html',
+    'mime_type': 'text/html'
+  },
+  '/style.css': {
+    'file': 'style.css',
+    'mime_type': 'text/css'
+  }
+
+}
+
 const server = http.createServer((req, res) => {
   const method = req.method
   const route = url.parse(req.url).pathname
-  // this is sloppy, espcially with more assets, create a "router"
-  if (route === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(findAsset('index.html'))
-    logRequest(method, route, 200)
-    res.end()
-  } else {
-    // missing asset should not cause server crash
-    throw new Error('route not found')
-    res.end()
+  console.log('outside async');
+
+  const handleRequest = async () => {
+    console.log('inside async')
+    try {
+      console.log('inside try')
+      res.writeHead(200, {'Content-Type': router[route]['mime_type']})
+      await res.write(findAsset(router[route]['file']))
+      console.log('after await')
+      logRequest(method, route, 200)
+      res.end()
+    } catch (e) {
+      console.log(e)
+      res.writeHead(404, {'Content-Type': 'text/html'})
+      res.write('404')
+      logRequest(method, route, 404)
+      res.end()
+    }
   }
-  // most important part, send down the asset
+  handleRequest()
+
 })
 
 server.listen(port, hostname, () => {
