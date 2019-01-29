da906cfbb54ac4c85cc20de421cc6da42a105d51
McNamara <cmcnamara@cmcnamarambpxx.cnet.cnwk>
2019-01-28 17:57:55 -0800

    debugging and testing exercise

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,24 @@
+users = require ('./users')
+
 // write some tests
 describe('users', () => {
-  
+
+  test('findUser to work with valid id', () => {
+    return users.findUser(1).then(data => {
+      expect(data ).toHaveProperty('id', 1)
+    })
+  })
+  test('findUser to fail with invalid id', () => {
+    return users.findUser(30).catch(e => expect(e).toMatch("No user with id 30"))
+  })
+
+  test('deleteUser', () => {
+    return users.deleteUser(3).then(data => {
+      expect(data).toEqual({"id": 3})
+    })
+  })
+  test('deleteUser to fail with invalid id', () => {
+    return users.deleteUser(30).catch(e => expect(e).toMatch("No user with id 30"))
+  })
+
 })
--- exercises/testing/users.js
+++ exercises/testing/users.js
@@ -9,25 +9,34 @@ const users = new Array(20).fill(0)
 
 // simulate async db call with promise
 const findUser = (id) => new Promise((resolve, reject) => {
+  // console.log(users)
   const user = users.find(user => user.id === id)
   if (user) {
     return resolve(user)
   }
   reject(new Error(`No user with id "${id}"`))
-})
+}).catch(
+  (error) => {
+    // console.log(error)
+  }
+)
 
 // simulate async db call with promise
 const deleteUser = (id) => new Promise((resolve, reject) => {
-  const id = fixId(id)
+  // const id = fixId(id)
   const i = users.findIndex(user => user.id === id)
 
   if (i < 0) {
     return reject(new Error(`No user with id "${id}"`))
   }
 
-  users.slice(i, 1)
+  users.splice(i, 1)
   resolve({id})
-})
+}).catch(
+  (error) => {
+    // console.log(error)
+  }
+)
 
 module.exports = {
   findUser,
